81438f0a5e946d723fb42230c4479ca070dc647e
Mário Kolenič <mkolenicml@gmail.com>
2022-04-09 17:56:30 +0200

    Pridané potrebné súbory

---
+++ PresentationLayer/src/main/java/vava/edo/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package vava.edo.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
