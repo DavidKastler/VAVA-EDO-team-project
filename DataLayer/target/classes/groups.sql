00a79db026615a9a25015f702bf396b17096871a
Peter Koszorús PC <p.koszorus27@gmail.com>
2022-04-21 11:16:38 +0200

    Removed targets from the remote repo

--- DataLayer/target/classes/groups.sql
+++
@@ -1,7 +0,0 @@
-insert into groups (gr_id, group_name, group_creator_id) values (1, 'Lotlux', 4);
-insert into groups (gr_id, group_name, group_creator_id) values (2, 'Opela', 2);
-insert into groups (gr_id, group_name, group_creator_id) values (3, 'Mat Lam Tam', 8);
-insert into groups (gr_id, group_name, group_creator_id) values (4, 'Bigtax', 8);
-insert into groups (gr_id, group_name, group_creator_id) values (5, 'Flowdesk', 8);
-insert into groups (gr_id, group_name, group_creator_id) values (6, 'Konklux', 1);
-insert into groups (gr_id, group_name, group_creator_id) values (7, 'Span', 5);
