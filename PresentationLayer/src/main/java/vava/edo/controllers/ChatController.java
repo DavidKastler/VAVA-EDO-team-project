28b5c13d42d9ed839bb6d27be21cd5987bcfd5af
Juraj Glatz <xglatz@stuba.sk>
2022-04-22 00:29:52 +0200

    chat lava strana hotova

--- PresentationLayer/src/main/java/vava/edo/controllers/ChatController.java
+++ PresentationLayer/src/main/java/vava/edo/controllers/ChatController.java
@@ -5,6 +5,7 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -22,6 +23,9 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
@@ -61,9 +65,9 @@ public class ChatController implements Initializable {
     public Button editButton(Button button, String username, Boolean color){
         button.setText(username);
         button.setWrapText(true);
-        Font font = Font.font("Arial", FontWeight.BOLD, 22);
+        Font font = Font.font("Arial", FontWeight.BOLD, 24);
         button.setFont(font);
-        button.setStyle("-fx-background-insets: 0 0 -1 0");
+        //button.setStyle("-fx-background-insets: 0 0 -1 0");
 
         if (color == true){
             button.setStyle("-fx-background-color: #c4c4c4");
@@ -74,8 +78,48 @@ public class ChatController implements Initializable {
         //button.setStyle("-fx-font-weight: bold");
         button.setPrefHeight(50);
         button.setPrefWidth(240);
+        button.setOnMouseClicked(event -> {
+            try {
+                handleViewChatButton(event);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
         return button;
+    }
+
+    public Label editLabel(Label label, String username){
+
+        return label;
+    }
+
+    public void refreshChatList(List<String> usernames){
+        for (int i = 0; i < usernames.size(); i++){
+            //System.out.println(id1);
+            Button button = new Button();
+            Boolean color;
+            String username;
+            if (i % 2 == 0){
+                color = false;
+            } else {
+                color = true;
+            }
+            editButton(button, usernames.get(i), color);
+
+
+            this.chat_list_pane.getChildren().add(button);
+        }
+    }
+
+    public void refreshColorsOfChats(ObservableList<Node> chats){
+        for (Integer i = 0; i < chats.size(); i++){
+            if ((i % 2) == 1){
+                chats.get(i).setStyle("-fx-background-color: #c4c4c4");
+            } else {
+                chats.get(i).setStyle("-fx-background-color:  #808080");
+            }
+        }
     }
 
     @Override
@@ -88,6 +132,8 @@ public class ChatController implements Initializable {
         } catch (IOException e) {
             System.out.println("Internet is not connected");
         }
+
+        /*
         //**** user 1 ***
         String id1 = "1234";
         String username1 = "Jano";
@@ -105,33 +151,47 @@ public class ChatController implements Initializable {
         String rememberMe2 = "";
         String isLogged2 = "";
         String lastActivity2 = "";
+        */
+
+        List<String> usernames = new ArrayList<>();
+        usernames.add("Jano");
+        usernames.add("Fero");
+        usernames.add("Kubo");
+        usernames.add("Lubo");
+        usernames.add("Eva");
+        usernames.add("Katka");
+        usernames.add("Hana");
+        usernames.add("Karol");
+        usernames.add("Adam");
+        usernames.add("Erzika");
+        usernames.add("Gizela");
+        usernames.add("Gustav");
+        usernames.add("Adolf");
 
 
-        for (int i = 1; i <= 12; i++){
-            //System.out.println(id1);
-            Button button = new Button();
-            Boolean color;
-            String username;
-            if (i % 2 == 0){
-                color = false;
-                username = username1;
-            } else {
-                color = true;
-                username = username2;
-            }
-            editButton(button, username1, color);
-
-
-            this.chat_list_pane.getChildren().add(button);
-        }
+        refreshChatList(usernames);
 
 
     }
 
     @FXML
     public void handleViewChatButton(MouseEvent mouseEvent) throws IOException {
+        refreshColorsOfChats(this.chat_list_pane.getChildren());
+
         //metoda na vratenie userov v danych chatoch/groupach
-    }
+        Button actualButton = ((Button)mouseEvent.getSource());
+        //System.out.println("Skuska");
+        for (Integer i = 0; i < this.chat_list_pane.getChildren().size(); i++){
+            if (actualButton.equals(this.chat_list_pane.getChildren().get(i))){
+                this.chat_list_pane.getChildren().get(i).setStyle("-fx-background-color:  #ff9797");
+            }
+        }
+
+        chat_name.setStyle("-fx-text-fill: #000000");
+        chat_name.setText(actualButton.getText());
+
+
+        }
 
     @FXML
     public void handleSearchChatButton(MouseEvent mouseEvent) throws IOException {
