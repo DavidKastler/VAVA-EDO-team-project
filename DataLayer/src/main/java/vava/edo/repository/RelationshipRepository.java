7c15bf25fae4ade35bfabc9fff305e43b224faa4
xbenam <xbenam@gmail.com>
2022-04-23 11:03:45 +0200

    Logs and enum fix

--- DataLayer/src/main/java/vava/edo/repository/RelationshipRepository.java
+++ DataLayer/src/main/java/vava/edo/repository/RelationshipRepository.java
@@ -11,12 +11,12 @@ public interface RelationshipRepository extends JpaRepository<Relationship, Inte
     @Query("select r from Relationship r where r.firstUser.uId = ?1 and r.secondUser.uId = ?2")
     Relationship findByFirstUserUIdAndSecondUserUId(Integer sender, Integer receiver);
 
-    @Query("select r from Relationship r where r.firstUser.uId = ?1 and r.status = 'pending'")
+    @Query("select r from Relationship r where r.firstUser.uId = ?1 and r.status = 'PENDING'")
     List<Relationship> findAllByUserUIdAndStatusIsPending(Integer userId);
 
-    @Query("select r from Relationship r where (r.firstUser.uId = ?1 or r.secondUser.uId = ?1) and r.status = 'accepted'")
+    @Query("select r from Relationship r where (r.firstUser.uId = ?1 or r.secondUser.uId = ?1) and r.status = 'ACCEPTED'")
     List<Relationship> findAllByUserIdAndStatusIsAccepted(Integer user);
 
-    @Query("select (count(r) > 0) from Relationship r where r.firstUser.uId = ?1 and r.secondUser.uId = ?2 and r.status = 'blocked'")
+    @Query("select (count(r) > 0) from Relationship r where r.firstUser.uId = ?1 and r.secondUser.uId = ?2 and r.status = 'BLOCKED'")
     Boolean existsByFirstUserUIdAndSecondUserUIdAndStatusIsBlocked(Integer senderId, Integer receiverId);
 }
