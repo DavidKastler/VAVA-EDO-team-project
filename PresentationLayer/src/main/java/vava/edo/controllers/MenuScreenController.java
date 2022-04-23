49a4a7e9584bb14474efb5a22ab4588037134277
Mário Kolenič <mkolenicml@gmail.com>
2022-04-23 16:12:17 +0200

    Spravené okná pre Profile a Settings

--- PresentationLayer/src/main/java/vava/edo/controllers/MenuScreenController.java
+++ PresentationLayer/src/main/java/vava/edo/controllers/MenuScreenController.java
@@ -3,6 +3,7 @@ package vava.edo.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import vava.edo.Handlers.TodoHandler;
 import vava.edo.controllers.models.CalendarScreenModel;
 import vava.edo.controllers.models.TodoScreenModel;
@@ -13,6 +14,15 @@ public class MenuScreenController {
 
     @FXML
     private HBox hBoxChangingScreen;
+
+    @FXML
+    private VBox vBoxProfileScreen;
+
+    @FXML
+    private VBox vBoxEditProfileScreen;
+
+    @FXML
+    private VBox vBoxSettingsScreen;
 
     public void setUser(User user){this.user = user;}
 
@@ -29,6 +39,37 @@ public class MenuScreenController {
     }
 
     public void handleUserButton(MouseEvent mouseEvent) {
+        hBoxChangingScreen.setDisable(true);
+
+        vBoxProfileScreen.setDisable(false);
+        vBoxProfileScreen.setVisible(true);
+    }
+
+    public void handleShowPassword(MouseEvent mouseEvent) {
+    }
+
+    public void handleCancelProfile(MouseEvent mouseEvent) {
+        vBoxProfileScreen.setDisable(true);
+        vBoxProfileScreen.setVisible(false);
+
+        hBoxChangingScreen.setDisable(false);
+    }
+
+    public void handleEditProfile(MouseEvent mouseEvent) {
+        vBoxProfileScreen.setDisable(true);
+
+        vBoxEditProfileScreen.setDisable(false);
+        vBoxEditProfileScreen.setVisible(true);
+    }
+
+    public void handleCancelProfileEdit(MouseEvent mouseEvent) {
+        vBoxEditProfileScreen.setDisable(true);
+        vBoxEditProfileScreen.setVisible(false);
+
+        vBoxProfileScreen.setDisable(false);
+    }
+
+    public void handleSaveProfile(MouseEvent mouseEvent) {
     }
 
     public void handleTodosButton(MouseEvent mouseEvent) {
@@ -48,5 +89,25 @@ public class MenuScreenController {
     }
 
     public void handleSettingsButton(MouseEvent mouseEvent) {
+        hBoxChangingScreen.setDisable(true);
+
+        vBoxSettingsScreen.setDisable(false);
+        vBoxSettingsScreen.setVisible(true);
+    }
+
+    public void handleCancelSettings(MouseEvent mouseEvent) {
+        vBoxSettingsScreen.setDisable(true);
+        vBoxSettingsScreen.setVisible(false);
+
+        hBoxChangingScreen.setDisable(false);
+    }
+
+    public void handleAbout(MouseEvent mouseEvent) {
+    }
+
+    public void handleFeedback(MouseEvent mouseEvent) {
+    }
+
+    public void handleLogout(MouseEvent mouseEvent) {
     }
 }
